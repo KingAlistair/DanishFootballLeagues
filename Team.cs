8dce365a1a4962a6f3038b3b37d76afac0ac29f8
Dániel Szabó <kingalistairlol@gmail.com>
2023-04-05 10:09:53 +0200

    Fixed the filereader in Team.cs, so it princt everything once

--- Team.cs
+++ Team.cs
@@ -1,4 +1,5 @@
-class Team {
+class Team
+{
 
     private int id;
     private string name;
@@ -10,31 +11,37 @@ class Team {
     {
     }
 
-    public static void ReadAllTeams(){
-
+    public static void ReadAllTeams()
+    {
         string filePath = @"files/teams.csv";
-
         StreamReader reader = null;
 
-      if (File.Exists(filePath)){
-         reader = new StreamReader(File.OpenRead(filePath));
+        if (File.Exists(filePath))
+        {
+            reader = new StreamReader(File.OpenRead(filePath));
+            List<string> listA = new List<string>();
 
-         List<string> listA = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
 
-         while (!reader.EndOfStream){
-            var line = reader.ReadLine();
-            var values = line.Split(',');
-            foreach (var item in values){
-               listA.Add(item);
+                foreach (var item in values)
+                {
+                    listA.Add(item);
+                }
             }
-            foreach (var coloumn1 in listA){
-               Console.WriteLine(coloumn1);
-            }
-         }
-      } else {
-         Console.WriteLine("File doesn't exist");
-      }
-      Console.ReadLine();
-   }
 
+            foreach (var column1 in listA)
+            {
+                Console.WriteLine(column1);
+            }
+        }
+        else
+        {
+            Console.WriteLine("File doesn't exist");
+        }
+
+        Console.ReadLine();
+    }
 }
